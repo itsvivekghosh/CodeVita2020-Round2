fe6760cd2764e9d0f41f9506ebdc1ccdea3ce31d
Vivek <soapmactevis1@gmail.com>
2020-07-18 12:37:32 +0530

    Added Answers

---
+++ PetrolPump.py
@@ -0,0 +1,54 @@
+''' 
+	Problem D: Petrol Pump
+'''
+
+class Solution(object):
+  	
+    def __init__(self):
+      	self.arr = list(map(int, input().split()))
+        self.difference = self.findDifference()
+
+        self.min_prob = (sum(self.arr) - self.difference) / 2
+        self.max_prob = min_prob + self.difference
+
+        pass
+      
+    def __del__(self):
+      	print(self.max_prob)
+      
+    def findSolution(self):
+
+        size = len(self.arr)-1
+        sumOfArray = sum(self.arr)
+
+        memory = [[0 for i in range(sumOfArray+1)] for j in range(sumOfArray+1)]
+
+        for i in range( + 1):
+            memory[i][0] = True
+
+        for i in range(1, sumOfArray+1):
+            dp[0][i] = False
+
+
+        for i in range(1, size+1):
+            for j in range(1, sumOfArray+1):
+
+                memory[i][j] = memory[i-1][j]
+
+                if self.arr[i-1] <= j:
+                    memory[i][j] |=memory[i-1][j-self.arr[i-1]]
+
+            for j in range(sumOfArray//2, -1, -1):
+
+                if memory[size] is True:
+                    diff = sumOfArray - (2*j)
+                    break
+            return
+      
+
+def main():
+  	pass
+  
+
+if __name__ is '__main__':
+  	main()
--- deathBattle.py
+++ deathBattle.py
@@ -1,106 +1,7 @@
-# import math
-
-# factorial = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 
-# 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000,
-# 355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000, 51090942171709440000
-# ]
-
-# def findEuclideanGCD(a, b):
-#     if (a == 0):
-#         return b
-#     return findEuclideanGCD(b % a, a)
-    
-# def PerformSubtraction(number1, number2):
-# 	numer = number1[0]*number2[1] - number2[0]*number1[1]
-# 	denom = number1[1]*number2[1]
-
-# 	num = numer//findEuclideanGCD(numer, denom)
-# 	den = denom//findEuclideanGCD(numer, denom)
-
-# 	return [num, den]
-
-
-# def self.PerformAddition(number1, number2):
-
-# 	numer = number1[0]*number2[1] - number2[0]*number1[1]
-# 	denom = number1[1]*number2[1]
-
-# 	num = numer//findEuclideanGCD(numer, denom)
-# 	den = denom//findEuclideanGCD(numer, denom)
-
-# 	return [num, den]
-
-# def PerformMultiplication(number1, number2):
-# 	numer = number1[0]*number2[0]
-# 	denom = number1[1]*number2[1]
-
-# 	num = numer//findEuclideanGCD(numer, denom)
-# 	den = denom//findEuclideanGCD(numer, denom)
-
-# 	return [num, den]
-
-# def PerformExponential(number1, size):
-# 	ans=[1, 1]
-# 	for i in range(size):
-# 		ans = PerformMultiplication(ans, number1)
-# 	return ans
-
-# def PerformCombination(n, r):
-# 	if n == r or r == 0:
-# 		return [1, 1]
-
-# 	if r == 1 or r == n-1:
-# 		return [n, 1]
-
-# 	numer = factorial[n]
-# 	denom =factorial[r] * factorial[n-r]
-
-# 	num = numer//findEuclideanGCD(numer, denom)
-# 	den = denom//findEuclideanGCD(numer, denom)
-
-# 	return [num, den]
-
-# T = int(input())
-# for case in range(T):
-# 	x, y, num1, num2, m, n = list(map(int, input().split()))
-
-# 	number1, number2 = 0, 0
-
-# 	if (x+n)*m < y:
-# 		print("RIP")
-# 		continue
-
-# 	if (x*m) >= y:
-# 		number1, number2 = 1, 1
-# 		numer = number1/(math.findEuclideanGCD(number1, number2)) ##
-# 		denom = number2/(math.findEuclideanGCD(number1, number2))
-
-# 		print("{}/{}".format(numer, denom))
-# 		continue
-
-# 	remaining = y - (x*m)
-# 	rLuckAttack = math.ceil(remaining/n)
-
-# 	currentFraction = [0, 1]
-# 	probabililityLucky = [num1//(findEuclideanGCD(num1, num2)), num2//(findEuclideanGCD(num1, num2))]
-# 	probabililityNotLucky = PerformSubtraction([1, 1], probabililityLucky)
-
-# 	for i in range(rLuckAttack, m+1):
-# 		currentProbability = PerformMultiplication(PerformCombination(m, i), PerformExponential(probabililityLucky, i))
-# 		currentNotProbability = PerformExponential(probabililityNotLucky, m-i)
-# 		currentProbability = PerformMultiplication(currentProbability, currentNotProbability)
-# 		currentFraction = self.PerformAddition(currentFraction, currentProbability)
-
-# 	print("{}/{}".format(currentFraction[0], currentFraction[1]))
-
-# """
-# 	2
-# 	10 33 7 10 3 2
-# 	10 999 7 10 3 2
-# """
-
+### Import Math Librar for math.ceil()
 import math
 
+### Factorial of 1 to 30 Numbers
 factorial = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 
 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000,
 355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000, 51090942171709440000, 1124000727777607680000,
@@ -109,6 +10,7 @@ factorial = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880,
 ] ### Factorials of First 30 Numbers:: Source==> https://www.mymathtables.com/numbers/100-factorial-tables-chart.html
 
 
+#### This class Contains the Pseudo Code of the Problem
 class Solution(object):
 
 	""" Function for Input Data and calculating
@@ -118,10 +20,12 @@ class Solution(object):
 	4. Current Fraction
 	"""
 	def __init__(self):
+		### Input Data into A, H, L1, L2 , M, C as A, H, num1, num2, M, c
 		A, H, num1, num2, M, C = list(map(int, input().split()))
 
 		number1, number2 = 0, 0
 
+		### Base Condition
 		if (A + C) * M < H:
 			print("RIP")
 			return
@@ -153,11 +57,12 @@ class Solution(object):
 	""" 
 	Using Euclidean GCD Algorithm for finding Greatest Common Divisor or HCF of two numbers
 	"""
-	def findEuclideanGCD(self, a, b):
+	def findEuclideanGCD(self, a, b): ### Snippet Taken from GFG
 	    if (a == 0):
 	        return b
 	    return self.findEuclideanGCD(b % a, a)
 	    
+
 	"""Function for subtraction """
 	def PerformSubtraction(self, number1, number2):
 		numer = number1[0] * number2[1] - number2[0]*number1[1]
@@ -167,6 +72,7 @@ class Solution(object):
 		den = denom//self.findEuclideanGCD(numer, denom)
 
 		return [num, den]
+
 
 	""" Function for Addition"""
 	def PerformAddition(self, number1, number2):
@@ -179,6 +85,7 @@ class Solution(object):
 
 		return [num, den]
 
+
 	""" Function for Multiplications"""
 	def PerformMultiplication(self, number1, number2):
 		numer = number1[0] * number2[0]
@@ -189,12 +96,14 @@ class Solution(object):
 
 		return [num, den]
 
+
 	"""Function for Performing Exponentials"""
 	def PerformExponential(self, number1, size):
 		ans=[1, 1]
 		for i in range(size):
 			ans = self.PerformMultiplication(ans, number1)
 		return ans
+
 
 	""" Function for calculating nCr (Combinations)"""
 	def PerformCombination(self, N, R):
@@ -211,6 +120,7 @@ class Solution(object):
 		den = denom//self.findEuclideanGCD(numer, denom)
 
 		return [num, den]
+
 
 	def __del__(self):
 		pass
